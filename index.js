39715febfab0769c40bcabe69b190311c85659d7
Emil Ajdyna <emil@ajdyna.com>
2019-09-20 09:37:40 +0200

    More tests for the hasExtension method

--- index.js
+++ index.js
@@ -229,7 +229,9 @@ module.exports = {
     if (!extension) {
       const index = path.lastIndexOf('.')
       if (index === -1) { return false }
-      return !!path.substr(index + 1)
+      const ending = path.substr(index + 1)
+      if (ending.startsWith('/') || ending.startsWith('\\')) { return false }
+      return !!ending
     }
     if (extension.startsWith('.')) { return path.endsWith(extension) }
     return path.endsWith(`.${extension}`)
